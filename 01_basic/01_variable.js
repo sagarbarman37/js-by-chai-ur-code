c00b5b34af7980b314fe26a13534c9fa3b354664
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-07-22 17:31:47 +0000

    we learn dataType and variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,30 @@
+const accountId = 4654845354;
+// assignment cannot be changed in const variable
+// accountId = 65436458;
+// console.log(accountId);
+
+let accountEmail = "sagar43@gmail.com";
+// console.log(accountEmail);
+accountEmail = "barman@gmail.com";
+// console.log(accountEmail);
+/*
+The let declaration declares re-assignable, 
+block-scoped local variables, optionally initializing each to a value.
+*/
+
+var accountPassword = "sagar";
+accountPassword = 12134;
+// console.log(typeof accountPassword);
+/*
+ Prefer not to use var 
+ becouse of issue in block scope and functional scope.
+*/
+
+accountCity = "bangalore";
+accountCity = "Delhi";
+// console.log(accountCity);
+
+// ek sath main anek output dikhane ke liye:
+// console.log(accountId, accountEmail, accountPassword, accountCity);
+// console.table([a, b, c])
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,27 @@
+let str = "string";
+console.log(str);
+console.log(typeof str); //string
+
+let num = 7242;
+console.log(num);
+console.log(typeof num); // number
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool); // boolean
+
+let box;
+console.log(box);
+console.log(typeof box); // undefined
+
+let box2 = null;
+console.log(box2);
+console.log(typeof box2); // object
+
+let big = 5432745384345n;
+console.log(big);
+console.log(typeof big); // bigint
+
+let symb = Symbol("sagar barman"); // Symbol keyword always start with capital S.
+console.log(symb);
+console.log(typeof symb); // symbol
