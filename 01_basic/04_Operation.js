1869b635226dc328dcad8b3533cb51113d92ee09
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-07-23 03:09:18 +0000

    learn about operator

---
+++ 01_basic/04_Operation.js
@@ -0,0 +1,36 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 3);
+// console.log(2 % 2);
+
+let str1 = "Hello";
+let str2 = " sagar";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(3 + "4");
+// console.log("3" + 4);
+// console.log("3" + 4 + 2);
+// console.log(3 + 4 + "2");
+
+let bool = true; // 1
+// console.log( + bool);  // convert to number
+
+let bool2 = false; // 0
+// console.log( + bool2);  // convert to number
+
+// increment
+// decrement
+
+let count = 10;
+++count; // prefix
+// count++; // postfix
+// console.log(count);
+
+// video no- 7
