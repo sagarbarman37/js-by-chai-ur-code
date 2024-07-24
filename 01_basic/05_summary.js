e9b6c19524559e79df3d7f9e0084c33bc2e2500f
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-07-24 02:27:34 +0000

    summary of data type

---
+++ 01_basic/05_summary.js
@@ -0,0 +1,37 @@
+// primitive data  type
+//  7 types = string, number, boolean, null, undefine, Symbol, bigInt
+
+// primitive main memory ka copy milta hain. agar aap change karte he
+// to main memory main change nehin  hota hain, copy main change hota hain.
+const str = "string";
+const score = 100;
+const isLoggedIn = false;
+const tempareture = null;
+let userEmail; // let userEmail = undefined;
+const id = Symbol("sagar");
+const bigNumber = 435786578087465n;
+
+// non-primitive data type / reference data type
+
+// Array, Objects, Functions
+
+const fruits = ["apple", "mango", "banana", "orange"];
+// console.log(fruits);
+// console.log(fruits[2]);
+// console.log(fruits.length);
+
+const student = {
+  fullName: "sagar barman",
+  age: 24,
+  isStudent: false,
+};
+// console.log(student);
+// console.log(student.fullName);
+
+const myfunc = function () {
+  console.log("sagar barman");
+};
+// console.log(myfunc());
+
+
+// console.log(typeof fruits);
