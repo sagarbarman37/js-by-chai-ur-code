11c1a57a7aa4db3823d44aed2ae7fc0e320c4063
Sagar Barman <sagarbarman141@gmail.com>
2024-08-09 23:02:11 +0530

    Add 02_basic

---
+++ 02_basic/array.js
@@ -0,0 +1,15 @@
+// Array
+
+const myArr = [1, 2, 3, 4, 5]
+const heros = ["shaktiman", "spyderman", "badman"]
+const myArr2 = new Array(1, 2, 3, 4, 5, 6)
+
+// Array methods
+
+myArr.push(6, 7)  // item added, end of the array.
+myArr.pop()  // item deleted, end of the array.
+// console.log(myArr);
+
+myArr.unshift(9) // item added, stat of the array.
+myArr.shift() // item delete, start of the array.
+// console.log(myArr);
--- README.md
+++
@@ -1,2 +0,0 @@
-# js-by-chai-ur-code
-A code repo for javaScript series at cai ur code
