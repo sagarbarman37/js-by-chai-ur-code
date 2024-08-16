516c86bc94833f4bf40ce09c7e114422849010c0
Sagar Barman <sagarbarman141@gmail.com>
2024-08-16 23:12:40 +0530

    while loop

---
+++ 05_iteration/whileLoop.js
@@ -0,0 +1,28 @@
+// while loop
+
+let index = 0;
+while (index <= 10) {
+  //   console.log(`value of index is ${index}`);
+  //   index++;
+  index = index + 2; // ek bar main 2 jump karana.
+}
+
+let myArray = ["flash", "batman", "supperman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  //   console.log(`my feb. supper hero ${myArray[arr]}`);
+  arr++;
+}
+
+// Do While Loop
+
+// let score = 0;
+let score = 11; // if score condition se bada hai to minimum one time run hoga.
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
+
+// video no - 28
